776bd931ba213edb470e27dfc5356c167399bcf6
Achanandhi24 <achanandhi.m@gmail.com>
2023-05-04 21:15:34 +0530

    ThirdDay

--- multiply.sh
+++ multiply.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 echo "Enter the First value :"
-
 read firstValue
-
 echo "Enter the second value : "
 read secondValue
-
 answer=$(($firstValue * $secondValue))
 echo "Answer is " $answer
 
