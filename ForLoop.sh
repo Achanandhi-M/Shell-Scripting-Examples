5751c657cd4b2c03e2b8317d6ef8a008e9a0d9ec
Achanandhi24 <achanandhi.m@gmail.com>
2023-05-05 21:19:07 +0530

    FourthDay

---
+++ ForLoop.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+count=0
+
+for count in 0 1 2 3 4 5 6 7 8
+do
+	echo "The count is $count"
+
+done
+
+echo "Finished"
+
+
---
+++ whileLoop.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+count=0
+
+while [ $count -lt 10 ]
+do
+	echo "The count is  $count"
+	count=$((count+1))
+done
+
+#do and done are keywords used for indicating the beginning and the end of the block of code
+
+echo "Finished"
+
+
+
+
+
+
+
+
+
+
