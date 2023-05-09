e285198f7c4557e73a6262cd2e843591fa72c3bc
Achanandhi24 <achanandhi.m@gmail.com>
2023-05-09 21:22:42 +0530

    EightDay

---
+++ subtraction.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Enter the First Number : "
+
+read number1
+
+echo "Enter the Second Number: "
+
+read number2
+
+
+result=$((number1-number2))
+
+echo "Result is $result "
+
+
+
+
+
+
+
