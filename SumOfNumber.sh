cdaa1353a285a7a221110d04363697d04dd5f60d
Achanandhi24 <achanandhi.m@gmail.com>
2023-05-16 20:23:04 +0530

    FourteenthDay

---
+++ SumOfNumber.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo "Enter the value : "
+
+read value
+
+sum=0
+
+for ((i=0; i<="$value"; i++))
+do
+	sum=$((sum + i))
+done
+
+echo "The sum of $value is : $sum"
+
+
+
+
+
+
+
+
+
+
+
+
+
