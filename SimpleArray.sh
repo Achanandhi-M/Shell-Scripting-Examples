e0dd0e68897c5fc050946ee01a4beeba87c555d6
Achanandhi24 <achanandhi.m@gmail.com>
2023-05-17 06:39:44 +0530

    SixteenthDay

---
+++ SimpleArray.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+
+array=( 1 2 3 4 5 ) #Don't use square bracket  Don't use comma
+
+
+#when accessing array elements, you need to use the curly braces {} around the index.	
+
+echo "The array elements are : ${array[@]}"
+
+echo "The array elements at 0th position is : ${array[0]}"
+
+echo "The array elements at 4th position is : ${array[4]}"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
