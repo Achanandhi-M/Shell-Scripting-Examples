a47f5cb47da9b45a6485b24778563376c69d1bb9
Achanandhi24 <achanandhi.m@gmail.com>
2023-05-22 06:24:56 +0530

    TwentyOneDay

---
+++ StringLength.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+
+echo "Enter the string value : "
+
+read string
+
+
+answer=${#string} #syntax for finding length of the string
+
+echo "String Length : $answer "
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
