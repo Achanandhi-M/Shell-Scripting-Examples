1617be835e3518a7b2276f26c9a98f63ee98ce33
Achanandhi24 <achanandhi.m@gmail.com>
2023-05-24 19:43:27 +0530

    TwentyThreeDay

---
+++ patternMatching.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo "Enter the string:"
+
+read string
+
+echo "The String value you entered :$string"
+
+
+if [[ name = *hello* ]]; then	
+	echo "Pattern 'hello' found in the string"
+
+
+else
+      echo "Pattern 'hello' not found in the string"
+
+fi
+
+
+
+
+
+
+
+
+
+
+
+
