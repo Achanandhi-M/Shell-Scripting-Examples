9150fc1099aa9ea60a8847d76bb976daf0e1f725
Achanandhi24 <achanandhi.m@gmail.com>
2023-05-29 19:21:35 +0530

    TwentyEigthDay

---
+++ Factorial.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+factorial() {
+
+	value=$1
+
+	if ((value == 0)); then 
+		echo  1
+	else
+		echo $((value * $(factorial "$((value-1))")))
+
+        fi
+}
+
+
+
+echo -n "Enter the value:"
+
+read value
+
+result=$(factorial "$value")
+
+echo "Factorial of $value is $result "
+
+
